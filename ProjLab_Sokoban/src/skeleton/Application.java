3eaab8c8d2369f2c72f90a64946653e963a8a9df
Izsó András Zsolt <izsoandras@sch.bme.hu>
2018-03-18 22:39:09 +0100

    TestCase2 finished, wall added to the end of TestCase1

--- ProjLab_Sokoban/src/skeleton/Application.java
+++ ProjLab_Sokoban/src/skeleton/Application.java
@@ -2,7 +2,7 @@ package skeleton;
 
 public class Application {
     public static void main(String[] args) {
-       TestCase15 test15 = new TestCase15();
-       test15.Test();
+       TestCase2 test2 = new TestCase2();
+       test2.Test();
     }
 }
