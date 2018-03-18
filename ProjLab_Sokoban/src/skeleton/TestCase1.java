a67b1be258a5e1e0daf8e3e278630419c71fdc72
Izsó András Zsolt <izsoandras@sch.bme.hu>
2018-03-18 22:21:17 +0100

    TestCase1 finished

--- ProjLab_Sokoban/src/skeleton/TestCase1.java
+++ ProjLab_Sokoban/src/skeleton/TestCase1.java
@@ -1,9 +1,32 @@
 package skeleton;
 
+import model.Direction;
+import model.Tile;
+import model.Wall;
+import model.Worker;
+
 public class TestCase1 implements TestCase{
 
     @Override
     public void Test() {
+        Logger.Init();
 
+        Worker worker = new Worker();
+        Logger.Register(worker);
+        Tile from = new Tile();
+        Logger.Register(from);
+        Tile to = new Tile();
+        Logger.Register(to);
+        Wall wall = new Wall();
+        Logger.Register(wall);
+
+        Direction dir = Direction.Right;
+        Logger.Register(dir);
+
+        worker.InitializeField(from);
+        from.SetNeighbour(dir, to);
+        to.SetNeighbour(dir.Opposite(), from);
+
+        worker.Move(dir);
     }
 }
