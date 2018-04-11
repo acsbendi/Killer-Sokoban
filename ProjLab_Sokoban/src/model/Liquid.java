89f300d7be9b5060ed126f2124bfc60ff8bac6d6
Márton <Mrton@192.168.0.220>
2018-04-11 17:28:30 +0200

    utilities and warehouse load half-implemented

--- ProjLab_Sokoban/src/model/Liquid.java
+++ ProjLab_Sokoban/src/model/Liquid.java
@@ -1,5 +1,35 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import util.Factory;
+
 public interface Liquid {
 double GetFriction();
+
+
+
+
+
+public static Liquid create(String type) {
+	Map<String, Factory<Liquid>> prototypes = new HashMap<>();
+	prototypes.put("Oil",new Factory<Liquid>() {
+
+		@Override
+		public Liquid create() {
+			return new Oil();
+		}
+		
+	});
+prototypes.put("Honey",new Factory<Liquid>() {
+
+		@Override
+		public Liquid create() {
+			return new Honey();
+		}
+		
+	});
+return prototypes.get(type).create();
+}
 }
