059d182d58b7007711f14e6217b254e8527fcc72
Gergo <tomcsanyigergely@gmail.com>
2018-04-17 10:44:02 +0200

    ServerMessage, ServerMessageType classes added.

---
+++ ProjLab_Sokoban/src/messages/ServerMessageType.java
@@ -0,0 +1,42 @@
+package messages;
+
+import java.util.TreeMap;
+
+public enum ServerMessageType {
+    RegisterResponse((byte)0),
+    LoginResponse((byte)1),
+    LogoutResponse((byte)2),
+    EnterResponse((byte)3),
+    LeaveResponse((byte)4),
+    CheckLevel((byte)5),
+    LevelData((byte)6),
+    GameStarted((byte)7),
+    WorkerMoved((byte)8),
+    OilPlaced((byte)9),
+    HoneyPlaced((byte)10),
+    GameFinished((byte)11),
+    ResultResponse((byte)12);
+
+    private byte value;
+    private static TreeMap<Byte, ServerMessageType> map;
+
+    static {
+        map = new TreeMap<>();
+        ServerMessageType[] values = ServerMessageType.values();
+        for(ServerMessageType value : values) {
+            map.put((byte)value.ConvertToInt(), value);
+        }
+    }
+
+    public static ServerMessageType Create(byte value) {
+        return map.get(value);
+    }
+
+    private ServerMessageType(byte value) {
+        this.value = value;
+    }
+
+    public int ConvertToInt() {
+        return value;
+    }
+}
