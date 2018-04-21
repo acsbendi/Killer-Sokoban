b126d01cdbec948b8dd70a7b55eed219e97c9fd7
Gergo <tomcsanyigergely@gmail.com>
2018-04-21 13:28:52 +0200

    SokobanServer edited.

--- ProjLab_Sokoban/src/client/network/NetworkHandler.java
+++ ProjLab_Sokoban/src/client/network/NetworkHandler.java
@@ -148,6 +148,7 @@ public class NetworkHandler {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            controllerLogic.Disconnected();
         }
         else {
             controllerLogic.OfflineFailure();
