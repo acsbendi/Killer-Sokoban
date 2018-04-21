bb112f48dbff2874d04568a1353c811527ffa9f3
Izsó András Zsolt <izsoandras@sch.bme.hu>
2018-04-21 13:58:56 +0200

    All commands implemented

--- ProjLab_Sokoban/src/client/ui/Commands/UserInputExecutorCommands/UIEList.java
+++ ProjLab_Sokoban/src/client/ui/Commands/UserInputExecutorCommands/UIEList.java
@@ -6,12 +6,20 @@ import client.ui.Commands.InvalidArgumentException;
 public class UIEList implements UIECommand {
     @Override
     public void Execute(UserInputExecutor executor, String[] args) throws InvalidArgumentException {
-
+        if(args.length < 2)
+            throw new InvalidArgumentException(2);
+        if(args.length > 2)
+            throw new InvalidArgumentException(3);
+        if("own".equals(args[1]))
+            executor.OwnResults();
+        else if("top".equals(args[1]))
+            executor.TopResults();
+        else throw new InvalidArgumentException(2);
     }
 
     @Override
     public String getHelp() {
-        return null;
+        return "No content here yet, check back later.";
     }
 
     @Override
