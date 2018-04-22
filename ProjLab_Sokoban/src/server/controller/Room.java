906c7913508e26fb89c3680d9444c5504beb5064
Gergo <tomcsanyigergely@gmail.com>
2018-04-22 14:02:45 +0200

    server edited

---
+++ ProjLab_Sokoban/src/server/controller/Room.java
@@ -0,0 +1,11 @@
+package server.controller;
+
+import common.model.Warehouse;
+import common.model.Worker;
+
+import java.util.ArrayList;
+
+public class Room {
+    Warehouse warehouse;
+    ArrayList<Worker> workers;
+}
