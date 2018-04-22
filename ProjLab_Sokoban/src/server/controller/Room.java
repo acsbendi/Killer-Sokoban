c511548e96169d956080dcd45f16bf23905714f6
Gergo <tomcsanyigergely@gmail.com>
2018-04-22 23:38:43 +0200

    server edited

--- ProjLab_Sokoban/src/server/controller/Room.java
+++ ProjLab_Sokoban/src/server/controller/Room.java
@@ -1,11 +1,12 @@
 package server.controller;
 
-import common.model.Honey;
-import common.model.Oil;
-import common.model.Warehouse;
-import common.model.Worker;
+import common.model.*;
 import common.util.Direction;
+import common.util.JsonManager;
+import common.util.Position;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -43,7 +44,17 @@ public class Room {
     }
 
     public void Initialize(int level_id) {
-        // todo
+        File file = JsonManager.ResolveFileId(level_id);
+        HashMap<Position, Field> pitch = new HashMap<>();
+        ArrayList<Box> boxes = new ArrayList<>();
+        try {
+            JsonManager.EnforceConfigFile(file, pitch, boxes, workers);
+            for(int i = 0; i < clients.size(); i++) {
+                map.put(clients.get(i), workers.get(i));
+            }
+        } catch (FileNotFoundException | ClassCastException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean EverybodyReady() {
