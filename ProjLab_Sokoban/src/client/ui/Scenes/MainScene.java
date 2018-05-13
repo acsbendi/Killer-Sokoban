44aee13a546d7697b69a8eae62e813de17a44942
Izsó András Zsolt <izsoandras@sch.bme.hu>
2018-05-13 19:34:29 +0200

    MainScene //by: Ádám

--- ProjLab_Sokoban/src/client/ui/Scenes/MainScene.java
+++ ProjLab_Sokoban/src/client/ui/Scenes/MainScene.java
@@ -1,6 +1,9 @@
 package client.ui.Scenes;
 
 import client.ui.GUI;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -8,7 +11,7 @@ import javafx.stage.Stage;
 import java.util.*;
 
 /**
- * 
+ *
  */
 public class MainScene extends SokobanScene {
 
@@ -19,22 +22,58 @@ public class MainScene extends SokobanScene {
     }
 
     /**
-     * 
+     *
      */
     private Button enterButton;
 
     /**
-     * 
+     *
      */
     private TextField roomSizeText;
 
     /**
-     * 
+     *
      */
     private Button logoutButton;
 
     /**
-     * 
+     *
      */
     private Button resultButton;
+
+
+
+
+    // STATIC CREATE
+    public static MainScene Create(Stage window, GUI gui) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameScene.class.getResource("mainScene.fxml"));
+        Parent root = fxmlLoader.load();
+        MainScene mainScene = fxmlLoader.getController();
+
+        mainScene.window = window;
+        mainScene.gui = gui;
+        mainScene.scene = new Scene(root,MIN_WIDTH,MIN_HEIGHT);
+
+        return mainScene;
+    }
+
+    public void Load() {
+        window.setScene(scene);
+        roomSizeText.setText("");
+    }
+
+    public void ResultButtonPressed() {
+        gui.MainScene_resultButtonPressed();
+    }
+
+    public void LogoutButtonPressed() {
+        gui.MainScene_logoutButtonPressed();
+    }
+
+    public void EnterButtonPressed() {
+        int roomSize = Integer.parseInt(roomSizeText.getText());
+        if(roomSize >= 2 && roomSize <=4)
+            gui.MainScene_enterButtonPressed(roomSize);
+    }
+
 }
