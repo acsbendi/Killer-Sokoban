f29032624a2672e45c9d2a72811830eda9dee6bb
Gergo <tomcsanyigergely@gmail.com>
2018-05-13 17:06:34 +0200

    LoginScene edited.

--- ProjLab_Sokoban/src/client/ui/Scenes/LoginScene.java
+++ ProjLab_Sokoban/src/client/ui/Scenes/LoginScene.java
@@ -1,8 +1,13 @@
 package client.ui.Scenes;
 
+import client.ui.GUI;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.*;
 
@@ -42,7 +47,34 @@ public class LoginScene extends SokobanScene {
      */
     private Button backButton;
 
+    // STATIC CREATE
+    public static LoginScene Create(Stage window, GUI gui) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameScene.class.getResource("loginScene.fxml"));
+        Parent root = fxmlLoader.load();
+        LoginScene loginScene = fxmlLoader.getController();
+
+        loginScene.window = window;
+        loginScene.gui = gui;
+        loginScene.scene = new Scene(root,MIN_WIDTH,MIN_HEIGHT);
+
+        return loginScene;
+    }
+
+    public void Load() {
+        userNameText.setText("");
+        passwordText.setText("");
+        message.setText("Enter username and password.");
+    }
+
+    private void loginButtonPressed() {
+        gui.LoginScene_loginButtonPressed(userNameText.getText(), passwordText.getText());
+    }
+
+    private void backButtonPressed() {
+        gui.LoginScene_backButtonPressed();
+    }
+
     public void LoginFailure(String errorMessage){
-        // TODO implement here
+        message.setText(errorMessage);
     }
 }
