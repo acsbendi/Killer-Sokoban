eaf61c81363e9bb1eeda3d1b57a3a3127e19806c
Gergo <tomcsanyigergely@gmail.com>
2018-05-13 22:48:51 +0200

    Commands folder deleted.

--- ProjLab_Sokoban/src/client/ui/Commands/InvalidArgumentException.java
+++
@@ -1,7 +0,0 @@
-package client.ui.Commands;
-/**Argumentum hiba jelzésére szolgáló kivétel, paraméterben az első hibás argumentum sorszámámt várja*/
-public class InvalidArgumentException extends Exception{
-    public InvalidArgumentException(int argumentNo){
-        super("Invalid argument " + argumentNo);
-    }
-}
