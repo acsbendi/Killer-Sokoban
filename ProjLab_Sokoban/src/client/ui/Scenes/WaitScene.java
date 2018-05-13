97b254d65046b1168e2975423c1210dc5a34f180
Gergo <tomcsanyigergely@gmail.com>
2018-05-13 17:13:33 +0200

    WaitScene edited.

--- ProjLab_Sokoban/src/client/ui/Scenes/WaitScene.java
+++ ProjLab_Sokoban/src/client/ui/Scenes/WaitScene.java
@@ -1,7 +1,12 @@
 package client.ui.Scenes;
 
+import client.ui.GUI;
 import client.ui.Scenes.SokobanScene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.util.*;
 
@@ -21,4 +26,24 @@ public class WaitScene extends SokobanScene {
      */
     private Button leaveButton;
 
+    public static WaitScene Create(Stage window, GUI gui) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameScene.class.getResource("waitScene.fxml"));
+        Parent root = fxmlLoader.load();
+        WaitScene waitScene = fxmlLoader.getController();
+
+        waitScene.window = window;
+        waitScene.gui = gui;
+        waitScene.scene = new Scene(root,MIN_WIDTH,MIN_HEIGHT);
+
+        return waitScene;
+    }
+
+    // default Load method used
+
+    // private user event handlers
+    private void leaveButtonPressed() {
+        gui.WaitScene_leaveButtonPressed();
+    }
+
+    // no network event handlers
 }
