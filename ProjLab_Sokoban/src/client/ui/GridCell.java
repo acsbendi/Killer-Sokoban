51ec794915943d8844b68f86b59394ac89753188
acsbendi <acsbendi28@gmail.com>
2018-05-14 16:05:08 +0200

    Finished adding comments to GridCell.

--- ProjLab_Sokoban/src/client/ui/GridCell.java
+++ ProjLab_Sokoban/src/client/ui/GridCell.java
@@ -9,25 +9,36 @@ import javafx.scene.layout.StackPane;
  */
 public class GridCell {
 
+    /** The width of the image/StackPane created by GridCell. */
     private static int width = 36;
+
+    /** The height of the image/StackPane created by GridCell. */
     private static int height = 36;
 
+    //TODO width and height dependant on user settings.
+
+    /** The field associated with the GridCell. */
     private final Field field;
+
+    /** StackPane containing the graphical representation of the field.
+     * This will be returned from the CreateImage method. */
     private StackPane imagePane;
 
     /**
      * Constructs a new GridCell associated with the specified field.
+     *
      * @param field The associated field.
      */
-    public GridCell(Field field){
+    public GridCell(Field field) {
         this.field = field;
     }
 
     /**
      * Creates the image showing the current state of the associated field.
+     *
      * @return StackPane containing the graphical representation of the field.
      */
-    public StackPane CreateImage() {
+    StackPane CreateImage() {
         imagePane = new StackPane();
 
         imagePane.setPrefWidth(width);
@@ -42,11 +53,12 @@ public class GridCell {
 
     /**
      * Check what kind of field is in the grid cell and puts its image onto the image pane.
+     *
      * @param type A character representing the type of the field.
      */
-    private void ProcessFieldView(char type){
+    private void ProcessFieldView(char type) {
         View view = new TileView();
-        switch (type){
+        switch (type) {
             case 'g':
                 view = new GoalTileView();
                 break;
@@ -76,11 +88,12 @@ public class GridCell {
 
     /**
      * Check what kind of placeholder is on the field (tile) and puts its image on the top of the image pane.
+     *
      * @param type A character representing the type of the placeholder.
      */
-    private void ProcessPlaceholderView(char type){
-        View view = null;
-        switch (type){
+    private void ProcessPlaceholderView(char type) {
+        View view;
+        switch (type) {
             case 'b':
                 view = new BoxView();
                 break;
@@ -98,11 +111,12 @@ public class GridCell {
 
     /**
      * Check what kind of liquid is on the field (tile) and puts its image on the top of the image pane.
+     *
      * @param type A character representing the type of the liquid.
      */
-    private void ProcessLiquidView(char type){
+    private void ProcessLiquidView(char type) {
         View view;
-        switch (type){
+        switch (type) {
             case 'o':
                 view = new OilView();
                 break;
